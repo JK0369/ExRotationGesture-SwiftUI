dc719a67cc020f9b18f4c5a165e61fb2bd9c27d5
jake <palatable7@naver.com>
2022-10-09 16:08:21 +0900

    Initial Commit

---
+++ ExStyle/ExStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExStyleApp.swift
+//  ExStyle
+//
+//  Created by 김종권 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ExStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
