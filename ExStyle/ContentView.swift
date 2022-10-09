3ff66b9be103ef520553bbdc34735ce8b0cf7f2b
jake <palatable7@naver.com>
2022-10-09 17:28:57 +0900

    Implement

--- ExStyle/ContentView.swift
+++ ExStyle/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  @State private var angle: Angle = .init(degrees: 20)
+  
+  var body: some View {
+    Text("Jake의 iOS 앱 개발 알아가기")
+      .foregroundColor(.blue)
+      .rotationEffect(angle)
+      .gesture(rotationGesture)
+  }
+  
+  private var rotationGesture: some Gesture {
+    RotationGesture()
+      .onChanged {
+        print($0)
+        angle = $0
+      }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
